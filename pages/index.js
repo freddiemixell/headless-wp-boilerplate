fdb4f6c6f07abccefad35286a3c7c0dc29557b63
freddiemixell <fmixell@gmail.com>
2018-08-13 21:25:43 -0400

    added index page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,78 @@
-import React from 'react'
-import Link from 'next/link'
+import Layout from "../components/Layout.js";
+import React, { Component } from "react";
+import fetch from "isomorphic-unfetch";
+import Link from "next/link";
+import PageWrapper from "../components/PageWrapper.js";
+import Menu from "../components/Menu.js";
+import { Config } from "../config.js";
 
-export default () => (
-  <ul>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
-    <li>
-      <Link
-        href={{pathname: '/posts', query: { id: '2' }}}
-        as='/posts/2'
-      >
-        <a>post #2</a>
-      </Link>
-    </li>
-  </ul>
-)
+const headerImageStyle = {
+    marginTop: 50,
+    marginBottom: 50
+};
+
+class Index extends Component {
+    static async getInitialProps(context) {
+        const pageRes = await fetch(
+            `${Config.apiUrl}/wp-json/postlight/v1/page?slug=welcome`
+        );
+        const page = await pageRes.json();
+        const postsRes = await fetch(
+            `${Config.apiUrl}/wp-json/wp/v2/posts?_embed`
+        );
+        const posts = await postsRes.json();
+        const pagesRes = await fetch(
+            `${Config.apiUrl}/wp-json/wp/v2/pages?_embed`
+        );
+        const pages = await pagesRes.json();
+        return { page, posts, pages };
+    }
+
+    render() {
+        const posts = this.props.posts.map((post, index) => {
+            return (
+                <ul key={index}>
+                    <li>
+                        <Link
+                            as={`/post/${post.slug}`}
+                            href={`/post?slug=${post.slug}&apiRoute=post`}
+                        >
+                            <a>{post.title.rendered}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+        });
+        const pages = this.props.pages.map((page, index) => {
+            return (
+                <ul key={index}>
+                    <li>
+                        <Link
+                            as={`/page/${page.slug}`}
+                            href={`/post?slug=${page.slug}&apiRoute=page`}
+                        >
+                            <a>{page.title.rendered}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+        });
+        return (
+            <Layout>
+                <Menu menu={this.props.headerMenu} />
+                <h1>{this.props.page.title.rendered}</h1>
+                <div
+                    dangerouslySetInnerHTML={{
+                        __html: this.props.page.content.rendered
+                    }}
+                />
+                <h2>Posts</h2>
+                {posts}
+                <h2>Pages</h2>
+                {pages}
+            </Layout>
+        );
+    }
+}
+
+export default PageWrapper(Index);
